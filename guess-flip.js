fe9063753397ed6690ac04d6d8e0b1581ec5f777
rvenkatraman02 <rvenka@ad.unc.edu>
2022-02-12 20:41:21 -0500

    Correcting error message

--- guess-flip.js
+++ guess-flip.js
@@ -12,8 +12,9 @@ const call = args.call;
 // argv['call']
 // const call = argv.call;
 
+
 if (call == "heads" || call == "tails") {
     console.log(flipACoin(call));
 }
-else if (call == null) console.log("Error: no input.\nUsage: node guess-flip --call[heads|tails]");
-else console.log("Error: incorrect input.\nUsage: node guess-flip --call[heads|tails]");
+else if (call == null) console.log("Error: no input.\nUsage: node guess-flip.js --call[heads|tails]");
+else console.log("Error: incorrect input.\nUsage: node guess-flip.js --call[heads|tails]");
