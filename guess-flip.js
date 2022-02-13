efa6919f7674753bb974cf88dab582d23741f61e
rvenkatraman02 <rvenka@ad.unc.edu>
2022-02-12 20:45:10 -0500

    changing arguments

--- guess-flip.js
+++ guess-flip.js
@@ -1,23 +1,17 @@
-// import minimist from 'minimist';
-import {createRequire} from 'module';
-const require = createRequire(import.meta.url);
+import minimist from 'minimist';
+// import {createRequire} from 'module';
+// const require = createRequire(import.meta.url);
 
 import { flipACoin } from './modules/coin.mjs';
 
-const args = require('minimist')(process.argv.slice(2));
-args['call'];
-const call = args.call;
+// const args = require('minimist')(process.argv.slice(2));
+// args['call'];
+// const call = args.call;
 
-// const argv = (minimist)(process.argv.slice(2));
-// argv['call']
-// const call = argv.call;
+const argv = (minimist)(process.argv.slice(2));
+argv['call']
+const call = argv.call;
 
 if (call == null) console.log("Error: no input.\nUsage: node guess-flip.js --call=[heads|tails]"); 
 else if (call != "heads" && call != "tails") console.log("Error: incorrect input.\nUsage: node guess-flip.js --call=[heads|tails]");
 else console.log(flipACoin(call));
-
-// if (call == "heads" || call == "tails") {
-//     console.log(flipACoin(call));
-// }
-// else if (call == null) console.log("Error: no input.\nUsage: node guess-flip.js --call[heads|tails]");
-// else console.log("Error: incorrect input.\nUsage: node guess-flip.js --call[heads|tails]");
